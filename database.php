3ddc475a0776a2b251f26837acb8fdf49661ec4e
Tucker <tsmccart@svsu.edu>
2018-09-14 13:11:19 -0400

    First commit

---
+++ database.php
@@ -0,0 +1,39 @@
+<?php
+class Database 
+{
+	// these are not the real passwords
+	// for real passwords, see file in ../database subdirectory
+	private static $dbName = 'crud_tutorial' ; 
+	private static $dbHost = 'localhost' ;
+	private static $dbUsername = 'root';
+	private static $dbUserPassword = 'root';
+	
+	private static $cont  = null;
+	
+	public function __construct() {
+		exit('Init function is not allowed');
+	}
+	
+	public static function connect()
+	{
+	   // One connection through whole application
+       if ( null == self::$cont )
+       {      
+        try 
+        {
+          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);  
+        }
+        catch(PDOException $e) 
+        {
+          die($e->getMessage());  
+        }
+       } 
+       return self::$cont;
+	}
+	
+	public static function disconnect()
+	{
+		self::$cont = null;
+	}
+}
+?>
